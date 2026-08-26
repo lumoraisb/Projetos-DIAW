b3b950bb1364fab29da352c7016095417d90b5b7
Luiza Morais Braga <luiza.braga@cnh.com>
2026-08-25 21:54:37 -0300

    Adiciona atividade de meteorologia

---
+++ src/main/java/com/example/Atividade_Metereologia/application/AtividadeMetereologiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Atividade_Metereologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeMetereologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeMetereologiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Atividade_Metereologia/AtividadeMetereologiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Atividade_Metereologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeMetereologiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
